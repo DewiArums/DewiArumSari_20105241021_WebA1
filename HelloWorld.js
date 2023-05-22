d3e7eabd1b66a49cbead1386d5373af5275c3540
DewiArums <125951864+DewiArums@users.noreply.github.com>
2023-05-22 07:40:48 +0700

    Add files via upload

---
+++ HelloWorld.js
@@ -0,0 +1,27 @@
+document.writeln("Hello World");
+
+//Perkenalan
+document.writeln("<p>");
+document.writeln("Nama = Dewi Arum");
+document.writeln("</br>");
+document.writeln("NIM = 20105241021");
+document.writeln("</br>");
+document.writeln("Kelas = A1");
+document.writeln("</br>");
+document.writeln(0b10101);
+document.writeln("</br>");
+
+let fullName = "Dewi Arum Sari";
+let fisrtName = "Dewi";
+let middleName = "Arum";
+let lastName = "Sari"
+
+document.writeln("Nama lengkap : " + fullName);
+document.writeln("</br>");
+document.writeln("Nama pertama : " + fisrtName);
+document.writeln("</br>");
+document.writeln("Nama tengah : " + middleName);
+document.writeln("</br>");
+document.writeln("Nama belakang : " + lastName);
+
+
